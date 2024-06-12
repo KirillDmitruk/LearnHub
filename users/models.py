5c339ef15cb999e0be89389de8b2123df3ac6614
Kirill Dmitruk <Dmitruk.ks@mail.ru>
2024-06-12 16:21:25 +0300

    add payment

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 NULLABLE = {"null": True, "blank": True}
 
 
@@ -32,3 +34,34 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payment", **NULLABLE)
+    paid_course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        verbose_name="paid_course",
+        related_name="paid_course",
+        **NULLABLE,
+    )
+    paid_lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        verbose_name="paid_lesson",
+        related_name="paid_lesson",
+        **NULLABLE,
+    )
+
+    data = models.DateTimeField(
+        auto_now_add=True, verbose_name="Дата оплаты", **NULLABLE
+    )
+    payment_count = models.PositiveIntegerField(verbose_name="Сумма оплаты")
+    payment_method = models.CharField(max_length=50, verbose_name="Метод оплаты")
+
+    def __str__(self):
+        return f"{self.user} - {self.paid_course if self.paid_course else self.paid_lesson}"
+
+    class Meta:
+        verbose_name = "оплата"
+        verbose_name_plural = "оплаты"
