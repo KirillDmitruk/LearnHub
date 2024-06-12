3aff4040ab011e1a59fb57ed514207ad290b534f
Kirill Dmitruk <Dmitruk.ks@mail.ru>
2024-06-12 16:34:44 +0300

    add filter

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "users",
     "materials",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]
+}
---
+++ users/urls.py
@@ -0,0 +1,31 @@
+
+from django.urls import path
+
+from users.apps import UsersConfig
+from users.views import (
+    UserCreateAPIView,
+    UserListAPIView,
+    UserRetrieveAPIView,
+    UserUpdateAPIView,
+    UserDestroyAPIView, PaymentCreateAPIView, PaymentListAPIView, PaymentRetrieveAPIView, PaymentUpdateAPIView,
+    PaymentDestroyAPIView,
+)
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    # users
+    path("users/create/", UserCreateAPIView.as_view(), name="users_create"),
+    path("users/", UserListAPIView.as_view(), name="users_list"),
+    path("users/<int:pk>/", UserRetrieveAPIView.as_view(), name="users_detail"),
+    path("users/update/<int:pk>/", UserUpdateAPIView.as_view(), name="users_update"),
+    path("users/delete/<int:pk>/", UserDestroyAPIView.as_view(), name="users_delete"),
+    # payments
+    path("payment/create/", PaymentCreateAPIView.as_view(), name="payment_create"),
+    path("payment/", PaymentListAPIView.as_view(), name="payment_list"),
+    path("payment/<int:pk>/", PaymentRetrieveAPIView.as_view(), name="payment_detail"),
+    path("payment/update/<int:pk>/", PaymentUpdateAPIView.as_view(), name="payment_update"),
+    path(
+        "payment/delete/<int:pk>/", PaymentDestroyAPIView.as_view(), name="payment_delete"
+    ),
+]
