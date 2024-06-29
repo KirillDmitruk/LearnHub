cc9a3244d6307cfc9a714a0be1d077a7b3c4a174
Kirill Dmitruk <Dmitruk.ks@mail.ru>
2024-06-29 15:49:51 +0300

    updated block_users

--- users/tasks.py
+++ users/tasks.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 
 from celery import shared_task
+from dateutil.relativedelta import relativedelta
 from django.utils import timezone
 
 from users.models import User
@@ -9,10 +10,12 @@ from users.models import User
 @shared_task
 def block_users():
     """Блокировка неактивных пользователей"""
-    users = User.objects.exclude(last_login__isnull=True)
+
     now = timezone.now()
-    for user in users:
-        if now - user.last_login > timedelta(days=31):
-            user.is_active = False
-            user.save()
-            print(f"Пользователь {user.email} заблокирован.")
+    month_ago = now - relativedelta(months=1)
+    qs = User.objects.filter(
+        is_active=True,
+        last_login__lte=month_ago
+    )
+    qs.update(is_active=False)
+    print(f"Пользователь заблокирован.")
